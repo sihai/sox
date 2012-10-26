253a196e97feb78feb8e5b28e84fd9fb40371727
sihai <iac-rq@live.cn>
2012-10-27 04:41:21 +0800

    sox

---
+++ src/test/java/com/ihome/sox/store/CookieStoreTest.java
@@ -0,0 +1,24 @@
+/*
+ * ihome inc.
+ * soc
+ */
+package com.ihome.sox.store;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.ihome.sox.session.DefaultSessionManager;
+
+/**
+ * 
+ * @author sihai
+ *
+ */
+public class CookieStoreTest {
+
+	@Test
+	public void test() {
+	}
+}
